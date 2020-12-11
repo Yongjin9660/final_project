c237e2f30d08e4040d4e971d764c622eb856c887
Yongjin Kim <dydwls0669@ajou.ac.kr>
2020-12-12 01:39:14 +0900

    Sort Review

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -21,7 +21,7 @@ class Detail extends React.Component {
         }
     }
     textChange = (e) => {
-        if(e.target.value.length > 1000){
+        if (e.target.value.length > 1000) {
             alert("1000자 이하로 등록해 주세요");
             return;
         }
@@ -42,8 +42,8 @@ class Detail extends React.Component {
                 // this.setState({ contents: contents, showContents: contents, isLoading: false, dataNumber: data.data.length });
                 // this.setState({ contents: contents, isLoading: false, dataNumber: data.data.length });
                 this.props.dispatchContents(data.data);
-                for(let i=0;i<data.data.length;i++){
-                    if(data.data[i]._id === this.props.location.state.content_id){
+                for (let i = 0; i < data.data.length; i++) {
+                    if (data.data[i]._id === this.props.location.state.content_id) {
                         this.setState({ content: data.data[i], isLoading2: false });
                         break;
                     }
@@ -84,7 +84,7 @@ class Detail extends React.Component {
             alert("로그인 후 이용해주세요");
             return;
         }
-        if(this.state.rating === 0){
+        if (this.state.rating === 0) {
             alert("평점을 입력해주세요");
             return;
         }
@@ -96,8 +96,8 @@ class Detail extends React.Component {
             alert("최소 10자 이상 작성해주세요");
             return;
         }
-        for(let i =0 ; i<this.state.reviews.length;i++){
-            if(this.state.reviews[i].email === this.props.state.email){
+        for (let i = 0; i < this.state.reviews.length; i++) {
+            if (this.state.reviews[i].email === this.props.state.email) {
                 alert("이미 리뷰를 등록했습니다");
                 return;
             }
@@ -110,29 +110,33 @@ class Detail extends React.Component {
             rating: this.state.rating,
             reviewText: this.state.reviewText
         })
-        .then(function (res) {
-            console.log(res);
-            
-            alert("리뷰를 등록하였습니다.");
-            window.location.reload(true);
-        })
-        .catch(err => console.log('error : ', err));
+            .then(function (res) {
+                console.log(res);
+
+                alert("리뷰를 등록하였습니다.");
+                window.location.reload(true);
+            })
+            .catch(err => console.log('error : ', err));
+    }
+
+    sortReview = (a, b) => {
+        if(a.email === this.props.state.email)
+            return -1;
+        else
+            return 1;
     }
 
     render() {
-        // const { content } = this.props.location.state;
         const { content } = this.state;
-        
         console.log(this.state.content);
-        // console.log(this.props.state.contents);
-        // console.log(this.props);
-        
+     
 
-            return (
-                <div className="Detail">
-                    {this.state.isLoading2 ? 
-                    (<div></div>):
-                    (<div className="content_info_area">
+
+        return (
+            <div className="Detail">
+                {this.state.isLoading2 ?
+                    (<div className="Loading"><span>Loading...</span></div>)
+                    : (<div className="content_info_area">
                         <div className="poster">
                             <img src={content.url} title={content.title} alt={content.title} />
                         </div>
@@ -143,7 +147,7 @@ class Detail extends React.Component {
                                 <div className={"age" + content.movieRating}>{content.movieRating}</div>
                             </h1>
                             <Star rating={content.rating} />
-                            <div className="rating">{content.rating}</div>
+                            <div className="rating">{content.rating.toFixed(2)}</div>
                             <div className="ratingNumber">{content.ratingNumber}명 참여</div>
                             <br />
 
@@ -162,53 +166,57 @@ class Detail extends React.Component {
 
                             <div className="desc">{content.desc}</div>
                             <div className="clear"></div>
+                        </div> </div>)}
+
+                <div className="review">
+                    <div className="btn_star">
+                        <button type="button" className="btn_star1" id="1" title="1" onClick={this.clickStar}></button>
+                        <button type="button" className="btn_star2" id="2" title="2" onClick={this.clickStar}></button>
+                        <button type="button" className="btn_star1" id="3" title="3" onClick={this.clickStar}></button>
+                        <button type="button" className="btn_star2" id="4" title="4" onClick={this.clickStar}></button>
+                        <button type="button" className="btn_star1" id="5" title="5" onClick={this.clickStar}></button>
+                        <button type="button" className="btn_star2" id="6" title="6" onClick={this.clickStar}></button>
+                        <button type="button" className="btn_star1" id="7" title="7" onClick={this.clickStar}></button>
+                        <button type="button" className="btn_star2" id="8" title="8" onClick={this.clickStar}></button>
+                        <button type="button" className="btn_star1" id="9" title="9" onClick={this.clickStar}></button>
+                        <button type="button" className="btn_star2" id="10" title="10" onClick={this.clickStar}></button>
+                    </div>
+                    <div className="ReviewTextArea">
+                        <textarea value={this.state.reviewText} onChange={this.textChange} placeholder="리뷰를 작성해주세요. (최소 10자)"></textarea>
+                        <p className="review_length">{this.state.reviewText.length}/1000</p>
+                    </div>
+
+                    <button className="btn_createReview" onClick={this.makeReview}>리뷰 등록</button>
+                    <br />
+
+                    {this.state.isLoading ? (
+                        <div className="Loading">
+                            <span>Loading...</span>
                         </div>
-
-                    </div>)}
-
-                    <div className="review">
-                        <div className="btn_star">
-                            <button type="button" className="btn_star1" id="1" title="1" onClick={this.clickStar}></button>
-                            <button type="button" className="btn_star2" id="2" title="2" onClick={this.clickStar}></button>
-                            <button type="button" className="btn_star1" id="3" title="3" onClick={this.clickStar}></button>
-                            <button type="button" className="btn_star2" id="4" title="4" onClick={this.clickStar}></button>
-                            <button type="button" className="btn_star1" id="5" title="5" onClick={this.clickStar}></button>
-                            <button type="button" className="btn_star2" id="6" title="6" onClick={this.clickStar}></button>
-                            <button type="button" className="btn_star1" id="7" title="7" onClick={this.clickStar}></button>
-                            <button type="button" className="btn_star2" id="8" title="8" onClick={this.clickStar}></button>
-                            <button type="button" className="btn_star1" id="9" title="9" onClick={this.clickStar}></button>
-                            <button type="button" className="btn_star2" id="10" title="10" onClick={this.clickStar}></button>
-                        </div>
-                        <div className="ReviewTextArea">
-                            <textarea value={this.state.reviewText} onChange={this.textChange} placeholder="리뷰를 작성해주세요. (최소 10자)"></textarea>
-                            <p className="review_length">{this.state.reviewText.length}/1000</p>
-                        </div>
-
-                        <button className="btn_createReview" onClick={this.makeReview}>리뷰 등록</button>
-                        <br />
-
-                        {this.state.isLoading ? (
-                            <div className="Loading">
-                                <span>Loading...</span>
+                    ) : (
+                            <div className="reveiw_list">
+                                <ul>
+                                    {this.state.reviews.sort(this.sortReview).map(review => (
+                                       <Review
+                                            review={review}
+                                            _id={content._id}
+                                        /> 
+                                    ))}
+                                    {/* {this.state.reviews.map(review => (
+                                        <Review
+                                            review={review}
+                                            _id={content._id}
+                                        />
+                                    ))} */}
+                                </ul>
+                                <div className="clear"></div>
                             </div>
-                        ) : (
-                                <div className="reveiw_list">
-                                    <ul>
-                                        {this.state.reviews.map(review => (
-                                            <Review
-                                                review={review}
-                                                _id={content._id}
-                                            />
-                                        ))}
-                                    </ul>
-                                    <div className="clear"></div>
-                                </div>
-                            )
-                        }
-                    </div> 
+                        )
+                    }
                 </div>
-            );
-     
+            </div>
+        );
+
     }
 }
 
@@ -219,6 +227,6 @@ function mapDispatchToProps(dispatch) {
     return {
         dispatchContents: (contents) => dispatch(actionCreators.SetContents(contents))
     };
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Detail);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
